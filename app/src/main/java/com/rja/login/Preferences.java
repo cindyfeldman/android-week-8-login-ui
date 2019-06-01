8458d0be7148241fb4ac4e9dd6ba9c07c44c46e7
brandon <brandonrodmel@gmail.com>
2019-06-01 13:51:03 -0700

    done

--- app/src/main/java/com/rja/login/Preferences.java
+++ app/src/main/java/com/rja/login/Preferences.java
@@ -1,8 +1,24 @@
 package com.rja.login;
 
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+
 /**
  * Created by rjaylward on 2019-06-01
  */
 public class Preferences {
+    private static final String NAME = "login-Prefs";
+    private static final String USERNAME_KEY = "username";
+public static void setUsername(Context context, @Nullable String username){
+    context.getSharedPreferences(NAME,Context.MODE_PRIVATE)
+    .edit()
+            .putString(USERNAME_KEY,username)
+            .apply();
+}
+public static String getUsernames(Context context){
+   return  context.getSharedPreferences(NAME,Context.MODE_PRIVATE)
+            .getString(USERNAME_KEY,null);
+}
 
 }
--- app/src/main/java/com/rja/login/ui/login/LoginFragment.java
+++ app/src/main/java/com/rja/login/ui/login/LoginFragment.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.rja.login.Preferences;
 import com.rja.login.R;
 import com.rja.login.ui.main.MainActivity;
 
@@ -28,7 +33,41 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        TextInputEditText userNameView = view.findViewById(R.id.username_input_layout);
+        TextInputEditText passwordView = view.findViewById(R.id.password_input_layout);
 
+        MaterialButton logInButton = view.findViewById(R.id.LoginButton);
+
+        String lastUsername = Preferences.getUsernames(requireContext());
+        if (lastUsername != null) {
+            userNameView.setText(lastUsername);
+        }
+
+        logInButton.setOnClickListener(v -> {
+            validateUsernameAndPassword(userNameView, passwordView);
+            if (validateUsernameAndPassword(userNameView, passwordView)) {
+                Toast.makeText(getContext(), "Success", Toast.LENGTH_SHORT).show();
+                    onLogin();
+            } else {
+                Toast.makeText(getContext(), "Failure", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private boolean validateUsernameAndPassword(TextInputEditText username, TextInputEditText password) {
+        String usernameText = username.getText().toString() != null ? username.getText().toString() : "";
+        String passwordText = password.getText().toString() != null ? username.getText().toString() : "";
+
+        if (usernameText.length() < 3 || usernameText.length() > 25) {
+            username.setError("Username must be between 3 and 25 characters");
+            return false;
+        }
+        if (passwordText.length() < 3 || passwordText.length() > 25) {
+            return false;
+        }
+        Preferences.setUsername(requireContext(), usernameText);
+        return true;
     }
 
     private void onLogin() {
